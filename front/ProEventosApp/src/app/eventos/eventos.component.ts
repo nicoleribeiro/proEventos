7d0efe49df8014fbcacfddfe070eb9246a5519d9
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2023-03-28 20:34:27 -0300

    filtro funcionando

--- front/ProEventosApp/src/app/eventos/eventos.component.ts
+++ front/ProEventosApp/src/app/eventos/eventos.component.ts
@@ -9,28 +9,48 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   // medida em EM
   widthImg: number = 6;
   marginImg: number = 1;
   showImg: boolean = true;
   // Two-way data binding
-  filtroLista: string = '';
+  private _filtroLista: string = '';
 
-  constructor(
-    private http:HttpClient
-  ) { }
+  public getEventos():void{
+    this.http.get('https://localhost:7135/api/evento/').subscribe(
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
+      error => console.log(error));
+  }
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
-  showImage(){
-    this.showImg = !this.showImg;
+  filtrarEventos(filtrarPor: string): any{
+    // converter para maiúsculo
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
-  public getEventos():void{
-    this.http.get('https://localhost:7135/api/evento/').subscribe(
-      response => this.eventos = response,
-      error => console.log(error));
+  showImage(){
+    this.showImg = !this.showImg;
   }
 }
